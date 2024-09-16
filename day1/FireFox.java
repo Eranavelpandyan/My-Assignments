cfa75a2752f328b9acd4d668a3d2f82d1d42f886
Eranavelpandyan <eranavelpandyan@gmail.com>
2024-09-16 17:52:53 +0530

    Day 1

---
+++ day1/FireFox.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class FireFox {
+	public static void main(String[] args) {
+		float browserVersion=100.2f;
+		String browserName = "firefox";
+		boolean isVisible=true;
+		int releaseYear=1998;
+		char browserLogo='f';
+		System.out.println("Browser version: " +browserVersion);
+		System.out.println("Browser Name:" +browserName);
+		System.out.println("Able to open browser:" +isVisible);
+		System.out.println("Browser released Year:" +releaseYear);
+		System.out.println("Browser Logo:" +browserLogo);
+		
+		
+	}
+}
---
+++ day1/LearnIfElse.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class LearnIfElse {
+
+	public static void main(String[] args) {
+
+      int num=5;
+      //if.condition ->if - Press ctrl+space
+      if (num>0) {
+    	  System.out.println("positive number :"+num);
+      }else {
+    	  System.out.println("Number negative :+num");
+      }
+
+	}
+
+}
